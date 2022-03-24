54d7c1f556dd996a1c5ec37d6122dc9e79fe1141
Samir <Samir@DESKTOP-LVMN699>
2022-03-24 16:29:33 -0700

    edited table

--- Database/Database/TestTable.sql
+++ Database/Database/TestTable.sql
@@ -1,6 +1,7 @@
-﻿CREATE TABLE [dbo].[TestTable]
+﻿Alter TABLE [dbo].[TestTable]
 (Currency_Code Varchar(3) NOT NULL PRIMARY KEY,
 Currency_Name varchar(50) NULL,
-Currency_Description varchar(50) Null
+Currency_Description varchar(50) Null,
+Currency_rate int
 
 )
